5263a05457dc8525d2ebf86541e5af1add81e8b7
Marcus Martins <marcusmartins38@gmail.com>
2020-07-03 10:52:15 -0400

    Created and Initialize the Project

---
+++ src/pages/TelaPagamento/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Container } from "./styles";
+
+const TelaPagamento: React.FC = () => {
+  return (
+    <Container>
+      <h1>TelaPagamento</h1>
+    </Container>
+  );
+};
+
+export default TelaPagamento;
